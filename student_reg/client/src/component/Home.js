2801c5dfecaeafa37446d06c44eeec6f24417d77
HARI SAI PARASA <harisaiparasa@gmail.com>
2024-03-14 11:39:51 +0530

    Intro Updation

--- student_reg/client/src/component/Home.js
+++ student_reg/client/src/component/Home.js
@@ -3,8 +3,22 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='container mt-5'>
-      <h1 className='mt-5 text-center'>WELCOME TO MERN STACK PROJECT</h1>
-      
+      <h1 className='mt-5 text-center'>Welcome to my MERN Stack Student Registration System!
+</h1>
+      <br></br>
+      <h6 className=' text-center '>
+      This application allows you to manage your Student Data/Records efficiently using a MERN stack architecture.
+      </h6>
+        <br></br>
+      <h4 className='text-left' >Here's a breakdown of the technologies used:</h4>
+      <br></br>
+      <h5 >
+      <ul className='text-left' >This system provides functionalities for:</ul>
+      <li>Viewing all Students in your records.</li>
+      <li>Adding new Students with details like Name,Subject,Address,Contact etc.</li>
+      <li>Updating existing Student records to reflect any changes.</li>
+      <li>Deleting students from your records when necessary.</li>
+      </h5>
     </div>
   )
 }
